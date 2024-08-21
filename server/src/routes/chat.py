9fa0e8f01121d778be6a7aaf34eebdb61ddb7d76
Erick Singh Sandhu <essandhu22@gmail.com>
2024-08-21 08:28:19 -0700

    add to server and worker

--- server/src/routes/chat.py
+++ server/src/routes/chat.py
@@ -8,6 +8,8 @@ from ..redis.config import Redis
 from rejson import Path
 from ..schema.chat import Chat
 import time
+from ..redis.stream import StreamConsumer
+from ..redis.cache import Cache
 
 chat = APIRouter()
 manager = ConnectionManager()
@@ -43,9 +45,17 @@ async def token_generator(name: str, request: Request):
 # @route   POST /refresh_token
 # @desc    Route to refresh token
 # @access  Public
-@chat.post("/refresh_token")
-async def refresh_token(request: Request):
-    return None
+@chat.get("/refresh_token")
+async def refresh_token(request: Request, token: str):
+    json_client = redis.create_rejson_connection()
+    cache = Cache(json_client)
+    data = await cache.get_chat_history(token)
+
+    if data == None:
+        raise HTTPException(
+            status_code=400, detail="Session expired or does not exist")
+    else:
+        return data
 
 # @route   Websocket /chat
 # @desc    Socket for chatbot
@@ -56,15 +66,33 @@ async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_toke
     redis_client = await redis.create_connection()
     producer = Producer(redis_client)
     json_client = redis.create_rejson_connection()
+    consumer = StreamConsumer(redis_client)
 
     try:
         while True:
             data = await websocket.receive_text()
-            print(data)
             stream_data = {}
-            stream_data[token] = data
+            stream_data[str(token)] = str(data)
             await producer.add_to_stream(stream_data, "message_channel")
-            await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
-    
+            response = await consumer.consume_stream(stream_channel="response_channel", block=0)
+
+            print(response)
+            for stream, messages in response:
+                for message in messages:
+                    response_token = [k.decode('utf-8')
+                                      for k, v in message[1].items()][0]
+
+                    if token == response_token:
+                        response_message = [v.decode('utf-8')
+                                            for k, v in message[1].items()][0]
+
+                        print(message[0].decode('utf-8'))
+                        print(token)
+                        print(response_token)
+
+                        await manager.send_personal_message(response_message, websocket)
+
+                    await consumer.delete_message(stream_channel="response_channel", message_id=message[0].decode('utf-8'))
+
     except WebSocketDisconnect:
         manager.disconnect(websocket)
--- worker/main.py
+++ worker/main.py
@@ -3,31 +3,64 @@ import asyncio
 from src.model.gptj import GPT
 from src.redis.cache import Cache
 from src.schema.chat import Message
+from src.redis.stream import StreamConsumer
+import os
+from src.schema.chat import Message
+from src.redis.producer import Producer
 
 redis = Redis()
 
 async def main():
-
     json_client = redis.create_rejson_connection()
-    await Cache(json_client).add_message_to_cache(token="18196e23-763b-4808-ae84-064348a0daff", source="human", message_data={
-        "id": "3",
-        "msg": "I would like to go to the moon to, would you take me?",
-        "timestamp": "2022-07-16 13:20:01.092109"
-    })
-    data = await Cache(json_client).get_chat_history(token="18196e23-763b-4808-ae84-064348a0daff")
-    print(data)
+    redis_client = await redis.create_connection()
+    consumer = StreamConsumer(redis_client)
+    cache = Cache(json_client)
+    producer = Producer(redis_client)
 
-    message_data = data['messages'][-4:]
-    input = ["" + i['msg'] for i in message_data]
-    input = " ".join(input)
-    res = GPT().query(input=input)
+    print("Stream consumer started")
+    print("Stream waiting for new messages")
 
-    msg = Message(
-        msg=res
-    )
+    while True:
+        response = await consumer.consume_stream(stream_channel="message_channel", count=1, block=0)
 
-    print(msg)
-    await Cache(json_client).add_message_to_cache(token="18196e23-763b-4808-ae84-064348a0daff", source="bot", message_data=msg.dict())
+        if response:
+            for stream, messages in response:
+                # Get message from stream, and extract token, message data and message id
+                for message in messages:
+                    message_id = message[0]
+                    token = [k.decode('utf-8')
+                             for k, v in message[1].items()][0]
+                    message = [v.decode('utf-8')
+                               for k, v in message[1].items()][0]
+                    print(token)
+
+                    # Create a new message instance and add to cache, specifying the source as human
+                    msg = Message(msg=message)
+                    await cache.add_message_to_cache(token=token, source="human", message_data=msg.dict())
+
+                    # Get chat history from cache
+                    data = await cache.get_chat_history(token=token)
+
+                    # Clean message input and send to query
+                    message_data = data['messages'][-4:]
+                    input = ["" + i['msg'] for i in message_data]
+                    input = " ".join(input)
+                    res = GPT().query(input=input)
+                    msg = Message(
+                        msg=res
+                    )
+
+                    # Generate stream data to send to client
+                    stream_data = {}
+                    stream_data[str(token)] = str(msg.dict())
+                    await producer.add_to_stream(stream_data, "response_channel")
+
+                    # Add message to cache
+                    await cache.add_message_to_cache(token=token, source="bot", message_data=msg.dict())
+
+                # Delete messaage from queue after it has been processed
+                await consumer.delete_message(stream_channel="message_channel", message_id=message_id)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
