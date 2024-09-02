00e39c2259fdfbbacac235d75fc85b699a08b73a
Erick Sandhu <essandhu22@gmail.com>
2024-09-01 23:47:12 -0500

    remove old files

--- old_client/README.md
+++
@@ -1,3 +0,0 @@
-# Ask GPT
-
-Intuitive chat interface for receiving responses from GPT-J-6B
--- old_client/app/middleware/axios.tsx
+++
@@ -1,5 +0,0 @@
-import Axios from "axios";
-
-export const axios = Axios.create({
-  baseURL: "http://127.0.0.1:3000",
-});
